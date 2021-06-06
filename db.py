697ba0db4cfcf5a4f77fef3c70c942ba19dd7fad
xt <773540317@qq.com>
2021-06-06 23:42:54 +0800

    finish simple query

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import MySQLdb
-from MySQLdb._exceptions import OperationalError
+from MySQLdb._exceptions import OperationalError, ProgrammingError
 
 def db_login(user, passwd, server_addr, dbname):
     try:
@@ -26,6 +26,37 @@ def db_showtable(db):
     cursor.close()
 
     return res
+
+def table_showlines(db, table, search_conf):     # 显示 db table 的所有表项
+    cursor = db.cursor()
+
+    cursor.execute("describe " + table)
+    table_head = cursor.fetchall()
+    columns = []
+    for i in range(len(table_head)):
+        columns.append(table_head[i][0])
+    if search_conf == None or search_conf == {}:     # if search_conf 为空
+        try:
+            cursor.execute("SELECT * FROM " + table)
+        except OperationalError:
+            print("Not Such Table, Error!")
+            return [], []
+        result = cursor.fetchall()
+    else:
+        try:
+            WHERE_Clause = ''
+            for key, value in search_conf.items():
+                WHERE_Clause = WHERE_Clause + key + value + " AND "
+            WHERE_Clause = WHERE_Clause[:-5]            # 去掉最后的 and
+            cursor.execute("SELECT * FROM " + table + " WHERE " + WHERE_Clause)
+        except OperationalError or ProgrammingError:
+            print("Search Clause Error!")
+            return [], []
+        result = cursor.fetchall()
+
+    cursor.close()
+
+    return columns, result
 
 def db_close(db):
     if db is not None:
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import redirect
 from flask import request, make_response
 from flask import render_template
 from flask import url_for
+from flask import flash
 
 from db import *
 
@@ -13,7 +14,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.secret_key = 'lab3'
 
 # 对app执行请求页面地址到函数的绑定
-@app.route("/", methods=("GET", "POST"))
+@app.route("/", methods=("GET", "POST"))                # Python 装饰器, 使得 / 和 /login 都重定向到 login.html
 @app.route("/login", methods=("GET", "POST"))
 def login():
     """Log in a registered user by adding the user id to the session."""
@@ -44,7 +45,7 @@ def login():
 def table():
     # 出于简单考虑，每次请求都需要连接数据库，可以尝试使用其它context保存数据库连接
     if 'username' in session:
-        db = db_login(session['username'], session['password'], 
+        db = db_login(session['username'], session['password'],
                         session['ipaddr'], session['database'])
     else:
         return redirect(url_for('login'))
@@ -54,12 +55,59 @@ def table():
     db_close(db)
     if request.method == "POST":
         if 'clear' in request.form:
-            return render_template("table.html", rows = '', dbname=session['database'])
+            return render_template("table.html", rows = '', dbname=session['database'])         # 调用模板, 随后参数为键值对, 将表示模板中变量对应的具体值.
         elif 'search' in request.form:
+            return render_template("table.html", rows = tabs, dbname=session['database'])
+        elif 'select' in request.form:
+            # print(type(request.form))
+            session['table'] = request.form['select']
+            return redirect(url_for("lines"))
+        else:
+            print("No handler, Error!")
             return render_template("table.html", rows = tabs, dbname=session['database'])
 
     else:
         return render_template("table.html", rows = tabs, dbname=session['database'])
+
+@app.route("/table/lines", methods=(["GET", "POST"]))
+def lines():
+    # 出于简单考虑，每次请求都需要连接数据库，可以尝试使用其它context保存数据库连接
+    if 'username' in session:
+        db = db_login(session['username'], session['password'], session['ipaddr'], session['database'])
+    else:
+        return redirect(url_for('login'))
+
+    if 'table' not in session:
+        return redirect(url_for('table'))
+    table = session['table']
+    columns, rows = table_showlines(db, table, None)
+    
+
+    if request.method == "POST":
+        if 'clear' in request.form:
+            db_close(db)
+            return render_template("lines.html", columns = '', rows = '', tablename = table)         # 调用模板, 随后参数为键值对, 将表示模板中变量对应的具体值.
+        elif 'show all' in request.form:
+            db_close(db)
+            return render_template("lines.html", columns = columns, rows = rows, tablename = table)
+        elif "search" in request.form:
+            search_conf = dict()
+            for key, value in request.form.items():
+                if key != "search" and value != '':
+                    search_conf[key] = value
+            columns, rows = table_showlines(db, table, search_conf)
+            db_close(db)
+            return render_template("lines.html", columns = columns, rows = rows, tablename = table)
+        elif "Back" in request.form:
+            db_close(db)
+            return redirect(url_for("table"))
+        else:
+            db_close(db)
+            return render_template("lines.html", columns = columns, rows = rows, tablename = table)
+    else:
+        db_close(db)
+        return render_template("lines.html", columns = columns, rows = rows, tablename = table)
+
 
 # 测试URL下返回html page
 @app.route("/hello")
@@ -73,4 +121,5 @@ def not_found(e):
 
 if __name__ == "__main__":
 
-    app.run(host = "0.0.0.0", debug=True)
+    print(app.url_map)
+    app.run(host = "0.0.0.0", port=4000, debug=True)
